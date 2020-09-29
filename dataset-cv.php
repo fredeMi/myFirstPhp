ccff6a3810c5cc22e81d6bd40dd19a319bc5a82c
fredeMi <frederique.michelix@gmail.com>
2020-09-29 22:56:01 +0200

    indent file

--- dataset-cv.php
+++ dataset-cv.php
@@ -2,27 +2,31 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8" />
-    <title>Faux CV débuts php</title>
-    <meta name="description" content="2nd jour de php"/> 
-    <style>
-      body{
-        font-size: 15px;
-        font-weight: bold;
-        color: rgb(120, 100, 150);
-      }
-      h2{
-        text-decoration: underline;
-      }
-      table, th{
-        border: 1px solid black;
-        text-align: left;
-        padding: 3px;
-      }
-    </style>
+  <meta charset="UTF-8" />
+  <title>Faux CV débuts php</title>
+  <meta name="description" content="2nd jour de php" />
+  <style>
+    body {
+      font-size: 15px;
+      font-weight: bold;
+      color: rgb(120, 100, 150);
+    }
+
+    h2 {
+      text-decoration: underline;
+    }
+
+    table,
+    th {
+      border: 1px solid black;
+      text-align: left;
+      padding: 3px;
+    }
+  </style>
 </head>
+
 <body>
-<?
+  <?
 $cv = array(
     'nom' => 'Mauricette',
     'date_naissance' => '1979-06-10',
@@ -65,10 +69,10 @@ function formatDate($aDate){
   return strftime("%d %B %G", strtotime($aDate));
 }
 ?>
-<div>
-  <h1><?= $cv['nom'] ?> - <?= $cv['metier'] ?></h1>
-  <img src="img/<?= $cv['photo'] ?>" width=250 alt="<?= $cv['nom'] ?>">
-  <?function calculAge($birthDate){
+  <div>
+    <h1><?= $cv['nom'] ?> - <?= $cv['metier'] ?></h1>
+    <img src="img/<?= $cv['photo'] ?>" width=250 alt="<?= $cv['nom'] ?>">
+    <?function calculAge($birthDate){
     $time = strtotime($birthDate);
     if($time === false){
       return '';
@@ -83,47 +87,47 @@ function formatDate($aDate){
     return $year_diff;
 }
   ?>
-  <p><?= calculAge($cv['date_naissance'])?> ans</p>
-  <p><?= $cv['adresse'] ?></p>
-  <p>
-  <? $arrayTel = str_split($cv['tel'],2);
+    <p><?= calculAge($cv['date_naissance']) ?> ans</p>
+    <p><?= $cv['adresse'] ?></p>
+    <p>
+      <? $arrayTel = str_split($cv['tel'],2);
     foreach($arrayTel as $k=>$num){
       $tel=($k<4)?"$num-":"$num";
       echo $tel;
     }
   ?>
-  </p>
-</div>
-<h2>Diplômes</h2>
-<ul>
-  <? arsort($cv['diplomes']);
+    </p>
+  </div>
+  <h2>Diplômes</h2>
+  <ul>
+    <? arsort($cv['diplomes']);
   foreach($cv['diplomes'] as $place=>$year){?>
-  <li><?= $year ?> : <?= $place ?></li>
-  <?}?>
-</ul>
-<h2>Expériences</h2>
-<ul>
-  <?
+    <li><?= $year ?> : <?= $place ?></li>
+    <?}?>
+  </ul>
+  <h2>Expériences</h2>
+  <ul>
+    <?
   $experiences=array_reverse($cv['experiences']);
   foreach($experiences as $experience=>$info){
     $dateDebut=formatDate($info['debut']);
     $dateFin=($info['fin']=='now')?'<b>à maintenant</b>':'au '.formatDate($info['fin']);
     echo "<li>$dateDebut $dateFin: ".$info['libelle']."</li>";
   }?>
-</ul>
-<h2>Compétences</h2>
-<table>
-  <?foreach($cv['competences'] as $language=>$note){?>
-  <tr>
-    <th><?= $language ?></th>
-    <? for($i=1;$i<=5;$i++){
+  </ul>
+  <h2>Compétences</h2>
+  <table>
+    <?foreach($cv['competences'] as $language=>$note){?>
+    <tr>
+      <th><?= $language ?></th>
+      <? for($i=1;$i<=5;$i++){
         if ($i<=$note){
           echo '<td><img src="img/filled-star.svg"></td>';
         }
         else echo '<td><img src="img/emptied-star.svg"></td>';
     }
     ?>
-  </tr>
-  <?}?>
-</table>
+    </tr>
+    <?}?>
+  </table>
 </body>
