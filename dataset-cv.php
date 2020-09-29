3bdb1d055bf6c38183049bdcfa280a099a7469ed
fredeMi <frederique.michelix@gmail.com>
2020-09-29 22:55:01 +0200

    format age and tel + star images for languages

--- dataset-cv.php
+++ dataset-cv.php
@@ -1,9 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Faux CV débuts php</title>
+    <meta name="description" content="2nd jour de php"/> 
+    <style>
+      body{
+        font-size: 15px;
+        font-weight: bold;
+        color: rgb(120, 100, 150);
+      }
+      h2{
+        text-decoration: underline;
+      }
+      table, th{
+        border: 1px solid black;
+        text-align: left;
+        padding: 3px;
+      }
+    </style>
+</head>
+<body>
 <?
 $cv = array(
-    'nom' => 'Maurice',
-    'date_naissance' => '2000-12-10',
-    'adresse' => 'La Cantine 31000 Toulouse',
-    'tel' => '03131313131',
+    'nom' => 'Mauricette',
+    'date_naissance' => '1979-06-10',
+    'adresse' => 'Quai des savoirs 31400 Toulouse',
+    'tel' => '0531313131',
     'photo' => 'maurice.jpg',
     'metier' => 'As of the jungle developper',
     'diplomes' => array(
@@ -44,9 +68,31 @@ function formatDate($aDate){
 <div>
   <h1><?= $cv['nom'] ?> - <?= $cv['metier'] ?></h1>
   <img src="img/<?= $cv['photo'] ?>" width=250 alt="<?= $cv['nom'] ?>">
-  <p>age</p>
+  <?function calculAge($birthDate){
+    $time = strtotime($birthDate);
+    if($time === false){
+      return '';
+    }
+    $year_diff = '';
+    $date = date('d-m-Y', $time);
+    list($day,$month,$year) = explode('-',$date);
+    $year_diff = date('Y') - $year;
+    $month_diff = date('m') - $month;
+    $day_diff = date('d') - $day;
+    if ($day_diff < 0 || $month_diff < 0)$year_diff--;
+    return $year_diff;
+}
+  ?>
+  <p><?= calculAge($cv['date_naissance'])?> ans</p>
   <p><?= $cv['adresse'] ?></p>
-  <p><?= $cv['tel'] ?></p>
+  <p>
+  <? $arrayTel = str_split($cv['tel'],2);
+    foreach($arrayTel as $k=>$num){
+      $tel=($k<4)?"$num-":"$num";
+      echo $tel;
+    }
+  ?>
+  </p>
 </div>
 <h2>Diplômes</h2>
 <ul>
@@ -70,7 +116,14 @@ function formatDate($aDate){
   <?foreach($cv['competences'] as $language=>$note){?>
   <tr>
     <th><?= $language ?></th>
-    <td><?= $note ?></td>
+    <? for($i=1;$i<=5;$i++){
+        if ($i<=$note){
+          echo '<td><img src="img/filled-star.svg"></td>';
+        }
+        else echo '<td><img src="img/emptied-star.svg"></td>';
+    }
+    ?>
   </tr>
   <?}?>
 </table>
+</body>
